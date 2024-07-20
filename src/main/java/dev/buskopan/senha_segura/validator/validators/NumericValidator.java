f0da04ded381efb1b4a38c3baad33b6c4bfbdd87
= <=>
2024-07-20 13:00:20 -0300

    add numeric validator

---
+++ src/main/java/dev/buskopan/senha_segura/validator/validators/NumericValidator.java
@@ -0,0 +1,22 @@
+package dev.buskopan.senha_segura.validator.validators;
+
+import dev.buskopan.senha_segura.validator.Validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NumericValidator implements Validator {
+    @Override
+    public String validate(String pass) {
+        String regex = "(?=.*[0-9])";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pass);
+
+        if (!matcher.find()) {
+            return "Password must have at least one numeric character";
+        }
+
+        return null;
+
+    }
+}
