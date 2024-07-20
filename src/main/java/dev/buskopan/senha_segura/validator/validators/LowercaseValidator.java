54690ec1cfa5282aef1e47bd038629871b32499d
= <=>
2024-07-20 12:57:17 -0300

    add lowercase validator

---
+++ src/main/java/dev/buskopan/senha_segura/validator/validators/LowercaseValidator.java
@@ -0,0 +1,20 @@
+package dev.buskopan.senha_segura.validator.validators;
+
+import dev.buskopan.senha_segura.validator.Validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LowercaseValidator implements Validator {
+    @Override
+    public String validate(String pass) {
+        String regex = "(?=.*[a-z])";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pass);
+        if (!matcher.find()) {
+            return "Password must have at least one lowercase character";
+        }
+
+        return null;
+    }
+}
